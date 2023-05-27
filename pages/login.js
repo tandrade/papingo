b2b222ca3645de3709693607a34202028e1e98d5
Tamara Andrade <tamaraandrade@tamaras-mbp.lan>
2023-05-26 21:16:22 -0500

    Wire up google saml

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,32 @@
-export default function GettingStarted() {
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from 'next/router';
+
+export default function Login({ providers }) {
+  const { data: session } = useSession()
+  const router = useRouter();
+
+  if (session) {
     return (
-      <main>
-        <h1>Welcome to Papingo!</h1>
-        <h2>Login</h2>
-    </main>
-    );
+      <div>
+        <h1>Create Next App</h1>
+        <div>
+             <h2> Signed in as {session.user.email} <br /></h2> 
+              <div>
+              <button onClick={() => {
+                  signOut()
+              }}>
+                 Sign out
+             </button>
+              </div>
+        </div>
+      </div>
+
+    )
   }
+
+  return (
+    <div>
+      <button onClick={() => signIn('google')}>Sign in with gooogle</button>
+    </div>
+  );
+}
